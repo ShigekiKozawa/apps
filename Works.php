9120d9e07059aae0231ad6ed751895a6d6556089
ShigekiKozawa <sigekikozawa@aol.com>
2016-12-30 17:39:36 +0900

    Add files via upload

---
+++ Works.php
@@ -0,0 +1,132 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>NEW YOU</title>
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <!-- Animate.css -->
+  <link rel="stylesheet" href="css/animate.css">
+  <!-- Icomoon Icon Fonts-->
+  <link rel="stylesheet" href="css/icomoon.css">
+  <!-- Simple Line Icons-->
+  <link rel="stylesheet" href="css/simple-line-icons.css">
+  <!-- Magnific Popup -->
+  <link rel="stylesheet" href="css/magnific-popup.css">
+  <!-- Owl Carousel -->
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/owl.theme.default.min.css">
+  <!-- Salvattore -->
+  <link rel="stylesheet" href="css/salvattore.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="shortcut icon" href="favicon.ico">
+  <!-- Modernizr JS -->
+  <script src="js/modernizr-2.6.2.min.js"></script>
+</head>
+<body>
+  <?php require_once("header.php");?>
+
+		<div class="Left"><h1>Portfolio</h1><hr></div>
+		<div class="maincontents">
+		<div class="containers">
+		<div class="container">
+		<section class="content">
+		<ul class="sortlist">
+					<li class="current"><a href="#" class="pjax" data-page="works">全て</a></li>
+						<li><a class="pjax" data-page="works" href="Work_1.php" title="連載 に含まれる投稿をすべて表示">一年</a>
+						</li>
+						<li><a class="pjax" data-page="works" href="Work_2.php" title="作品_ソラリーマン に含まれる投稿をすべて表示">二年</a>
+						</li>
+						<li><a class="pjax" data-page="works" href="Work_3.php" title="作品_SGC に含まれる投稿をすべて表示">三年</a>
+						</li>
+				</ul>
+				<div class="works_posts">
+							<article class="works_post">
+								<a href="gengo.php" class="pjax" data-page="works">
+									<div class="thumb">
+
+										<img src="images/gengo.jpg" alt="初めてのWEBサイト作り"></div>
+									<h3>初めてのWEBサイト作り</h3>
+								</a>
+							</article>
+
+							<article class="works_post">
+								<a href="seiboon.php" class="pjax" data-page="works">
+									<div class="thumb">
+
+										<img src="images/mobility1.jpg" alt="Seiboon"></div>
+									<h3>Seiboon</h3>
+								</a>
+							</article>
+
+							<article class="works_post">
+								<a href="glowve.php" class="pjax" data-page="works">
+									<div class="thumb">
+
+										<img src="images/1_1.png" alt="Glowve"></div>
+									<h3>GLOWVE</h3>
+								</a>
+							</article>
+
+							<article class="works_post">
+								<a href="pythagoras.php" class="pjax" data-page="works">
+									<div class="thumb">
+
+										<img src="images/pythagoras.jpg" alt="pythagoras"></div>
+									<h3>ピタゴラ装置</h3>
+								</a>
+							</article>
+
+							<article class="works_post">
+								<a href="automato.php" class="pjax" data-page="works">
+									<div class="thumb">
+
+										<img src="images/automato.jpg" alt="Automato"></div>
+									<h3>オートマトン</h3>
+								</a>
+							</article>
+
+							<article class="works_post">
+								<a href="project.php" class="pjax" data-page="works">
+									<div class="thumb">
+
+										<img src="images/project1.jpg" alt="project"></div>
+									<h3>プロジェクト活動</h3>
+								</a>
+							</article>
+
+							<article class="works_post">
+								<a href="ucd.php" class="pjax" data-page="works">
+									<div class="thumb">
+
+										<img src="images/ucd1.JPG" alt="ucd"></div>
+									<h3>ユーザーセンタードデザイン</h3>
+								</a>
+							</article>
+						</div>
+ </section>
+ </div>
+</div>
+</div><!-- maincontents -->
+</div>
+
+
+
+  <?php require_once("footer.php");?>
+  <!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Magnific Popup -->
+	<script src="js/jquery.magnific-popup.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<!-- toCount -->
+	<script src="js/jquery.countTo.js"></script>
+	<!-- Main JS -->
+	<script src="js/main.js"></script>
+  </body>
+</html>
